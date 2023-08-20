0972844902359640328f202b879a3ef556625833
bulwark <bulwark@bulwark.bulwark>
2023-08-20 17:55:37 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,56 @@
-import Image from 'next/image'
+"use client"
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [questions, setQuestions] = useState<any[]>([]); // Explicitly set type to any[]
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setscore] = useState(0);
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      try {
+        const response = await fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple");
+        const result = await response.json();
+        setQuestions(result.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchdata();
+  }, []);
+
+  const handleNextClick = () => {
+    setCurrentQuestionIndex(prevIndex => prevIndex + 1);
+  };
+
+  if (questions.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  const currentQuestion = questions[currentQuestionIndex];
+  console.log(currentQuestion);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="flex bg-zinc-500 min-h-screen flex-col items-center justify-center pt-10">
+      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-white">
+        Online Quiz General Knowledge
+      </h2>
+      <div className='bg-zinc-700 flex-col h-[600px] w-[500px] text-green-500 px-4 flex-wrap'>
+        {currentQuestionIndex < questions.length ? (
+          <div className="flex flex-col  justify-between  items-center">
+            <p className="scroll-m-20 text-2xl font-semibold tracking-tight mt-8">Q{currentQuestionIndex + 1}</p>
+            <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight mt-8">{currentQuestion.question}</h1>
+            <button className='bg-zinc-800 text-2xl px-6  hover:bg-zinc-600 active:bg-zinc-800 focus:outline-none mt-16 w-[400px] py-2 first-letter: focus:ring focus:bg-zinc-300' onClick={() => setscore(score+1)}>{currentQuestion.correct_answer}</button>
+            <button className='bg-zinc-800 text-2xl px-6  hover:bg-zinc-600 active:bg-zinc-800 focus:outline-none focus:ring mt-3 w-[400px] py-2 focus:bg-zinc-300'>{currentQuestion.incorrect_answers[0]}</button>
+            <button className='bg-zinc-800 text-2xl px-6  hover:bg-zinc-600 active:bg-zinc-800 focus:outline-none focus:ring mt-3 w-[400px] py-2  focus:bg-zinc-300'>{currentQuestion.incorrect_answers[2]}</button>
+            <button onClick={handleNextClick} className='bg-zinc-500 mt-32 w-[400px] text-3xl'>Next</button>
+          </div>
+        ) : (
+          <div className="scroll-m-20 border-b pb-2 text-3xl  font-semibold tracking-tight transition-colors first:mt-0 text-green-600 ">
+            <h1 className='mt-64'>Quiz completed! Your score: {score}</h1>
+          </div>
+        )}
       </div>
     </main>
-  )
+  );
 }
